d6eefd3d6fb285a7955fef4e79ed57be540f555b
Max Hamulyak <max.hamulyak@gmail.com>
2018-03-24 13:25:26 +0100

    add example with multiple terms

--- Loops/Loops.java
+++ Loops/Loops.java
@@ -7,7 +7,8 @@ public class Loops {
         // Most loops are likely well defined, there are a few edge cases
         // variations of the following 5 examples are likely to be seen on the exam 
 
-        InfiniteLoop();
+        // InfiniteLoop();
+        MulitpleTermForStatement();
     }
 
     public static void InfiniteLoop() {
@@ -18,5 +19,25 @@ public class Loops {
         for( ; ; ) {
             System.out.println("Hello World");
         }
+    }
+
+    public static void MulitpleTermForStatement() {
+        int x = 0;
+        // note: still three sections in the loop
+        for (long y = 0, z = 4; x < 5 && y < 10; x++, y++) 
+        {
+            System.out.println(y + " ");
+            // PRINTS: 
+            // 0
+            // 1
+            // 2
+            // 3
+            // 4
+        }
+        System.out.println("X: " + x); // prints 5
+
+        // 1. You can declare a variable (x) before the loop begins, and use if after the loop ends
+        // 2. Your initialization block, boolean expression and update statements can include extra variables that may not referecne each other
+        // 3. The update statement can update multiple variables (x,y)
     }
 }
