497f4fb3173d1630da051dd1be829c2de5776472
Max Hamulyak <max.hamulyak@gmail.com>
2018-03-24 13:18:37 +0100

    add infinite loop example

--- Loops/Loops.java
+++ Loops/Loops.java
@@ -1,5 +1,22 @@
 public class Loops {
     public static void main(String[] args) {
         System.out.println("Hello, Loops");
+
+        // PAGE 81 of the Book mentions Loop Types
+
+        // Most loops are likely well defined, there are a few edge cases
+        // variations of the following 5 examples are likely to be seen on the exam 
+
+        InfiniteLoop();
+    }
+
+    public static void InfiniteLoop() {
+        // This looks like it will throw a compiler error.
+        // It is actually an infite loop
+        // Since each component for the for loop is optional (see the three sections ;)
+        // Without the three sections it will not compile
+        for( ; ; ) {
+            System.out.println("Hello World");
+        }
     }
 }
