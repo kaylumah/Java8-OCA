f0c7576489c21ccedba75dd39fe29d3b2df02bb5
Max Hamulyak <max.hamulyak@gmail.com>
2018-03-25 11:07:12 +0200

    add placeholder for 13 String methods

--- WorkingWithStrings/WorkingWithStrings.java
+++ WorkingWithStrings/WorkingWithStrings.java
@@ -1,5 +1,59 @@
 public class WorkingWithStrings {
     public static void main(String[] args) {
         System.out.println("Hello, WorkingWithStrings");
+        importantStringMethods();
+    }
+
+    private static void importantStringMethods() {
+        stringLength();
+        stringCharAt();
+        stringIndexOf();
+        stringSubstring();
+        stringUpperCaseAndLowerCase();
+        stringEqualsAndEqualsIgnoreCase();
+        stringStartsWithAndEndsWith();
+        stringContains();
+        stringReplace();
+        stringTrim();
+    }
+
+    private static void stringLength() {
+
+    }
+
+    private static void stringCharAt() {
+
+    }
+
+    private static void stringIndexOf() {
+
+    }
+
+    private static void stringSubstring() {
+
+    }
+
+    private static void stringUpperCaseAndLowerCase() {
+
+    }
+
+    private static void stringEqualsAndEqualsIgnoreCase() {
+
+    }
+
+    private static void stringStartsWithAndEndsWith() {
+
+    }
+
+    private static void stringContains() {
+
+    }
+
+    private static void stringReplace() {
+
+    }
+
+    private static void stringTrim() {
+
     }
 }
