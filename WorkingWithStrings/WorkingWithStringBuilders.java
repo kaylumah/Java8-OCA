3131e0de2002eb7e8d20fa40394d82dde04f0834
Max Hamulyak <max.hamulyak@gmail.com>
2018-04-02 10:56:49 +0200

    add basic stringbuilder example

--- WorkingWithStrings/WorkingWithStringBuilders.java
+++ WorkingWithStrings/WorkingWithStringBuilders.java
@@ -1,5 +1,19 @@
 public class WorkingWithStringBuilders {
     public static void main(String[] args) {
         System.out.println("WorkingWithStringBuilders");
+
+        String alpha = "";
+        for(char current = 'a'; current <= 'z'; current++) {
+            alpha += current;
+        }
+        System.out.println(alpha); // would create 27 objects
+
+
+        StringBuilder alphaBuilder = new StringBuilder();
+        for(char current = 'a'; current <= 'z'; current++) {
+            alphaBuilder.append(current);
+        }
+        System.out.println(alphaBuilder); // 1 object
+
     }
 }
